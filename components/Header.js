6bb363a6be9d72f02b054002209c9c46de37462f
github_username <princekumarsharma365@gmail.com>
2023-07-22 00:28:25 +0530

    set up for our project

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,8 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return(
+    <>
+    <h1 className="title">{title}</h1>
+    <h2>GrovNest Header Page</h2>
+    </>
+  ) 
 }
