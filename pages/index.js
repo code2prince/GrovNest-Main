1851e19fd0c7255328b2d2257c5b2fa9c9a7ecb2
github_username <princekumarsharma365@gmail.com>
2023-07-23 23:00:22 +0530

    navbar

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,17 @@ import Head from 'next/head'
 import Header from '@components/header/Header';
 import Footer from '@components/Footer'
 import Contact from '@components/contact/Contact';
+import Navbar from '@components/navbar/Navbar';
 
 export default function Home() {
   return (
     <div className="container">
+      
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Navbar/>
       <main>
         <Header title="Welcome to GrovNest" />
         <p className="description">
