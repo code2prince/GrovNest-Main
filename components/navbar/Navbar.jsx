e736526af99affbf0e1ef3d2b7110b8da613ef70
Rishab Jain <jainrishabbohra@gmail.com>
2023-07-28 22:06:10 +0530

    Navbar Updated

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,14 +1,22 @@
 import styles from "./Navbar.module.css";
+import { GrMenu } from "react-icons/gr";
 export default function Navbar() {
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.nav_center}>
           <div className={styles.nav_header}>
-            <h1>NAVBAR LOGO</h1>
-            <div className={styles.nav_logo}>GROVNEST</div>
+            {/* <img className={styles.logo} src="./grovelogo.png" alt="" /> */}
+            <h1>LOGO</h1>
+            {/* <GrMenu clasName={styles.toggle_btn} /> */}
           </div>
-          <div className={styles.nav_links}></div>
+          <div className={styles.nav_links}>
+            <div className={styles.nav_link}>Home</div>
+            <div className={styles.nav_link}>About</div>
+            <div className={styles.nav_link}>Contact</div>
+            <div className={styles.nav_link}>Services</div>
+            <div className={styles.nav_link}>Profile</div>
+          </div>
         </div>
       </nav>
     </>
