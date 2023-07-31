3ef59502fd5dd8817fe1d32f3a5ad25e6f19dbaf
Rishab Jain <jainrishabbohra@gmail.com>
2023-07-31 22:32:50 +0530

    Update Navbar

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import styles from "./Navbar.module.css";
 import { GrMenu } from "react-icons/gr";
-import Image from 'next/image';
+import Image from "next/image";
 export default function Navbar() {
+  const handleClick = (e) => {
+    console.log(e.target.classList);
+    e.target.classList.add("hide_nav")
+  }
   return (
     <>
       <nav className={styles.navbar}>
         <div className={styles.nav_center}>
           <div className={styles.nav_header}>
-            <img className={styles.logo} src="/grovN.jpg" alt="" />
-            {/* <h1>..</h1> */}
-            
-            {/* <GrMenu clasName={styles.toggle_btn} /> */}
+            <img className={styles.logo} src="/grovelogo.png" alt="" />
+            <img onClick={(e)=>{handleClick(e)}} id={styles.burger} style={{display:"none", width:"32px", height:"32px"}}  src="/menu1.jpg" alt="" />
           </div>
-          <div className={styles.nav_links}>
+          <div className={styles.hide_nav}>
             <div className={styles.nav_link}>Home</div>
             <div className={styles.nav_link}>About</div>
             <div className={styles.nav_link}>Contact</div>
             <div className={styles.nav_link}>Services</div>
-            <div className={styles.nav_link}>Profile</div>
           </div>
         </div>
       </nav>
