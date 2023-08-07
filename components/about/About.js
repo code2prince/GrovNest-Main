22ff1b5f80f9fd5baebb6e53a51fa73ea66dde28
github_username <princekumarsharma365@gmail.com>
2023-08-07 22:57:10 +0530

    about page designing

--- components/about/About.js
+++ components/about/About.js
@@ -6,15 +6,20 @@ export default function About(){
             <h2>About Us</h2>
             <h1>GrovNest</h1>
             <div className={styles.container}>
-                <div className={styles.vision}>
-                    <h2>Our Vision</h2>
-                    <p>GrovNest aims to incentivize & tokenization platform for its users to
+                <div>
+                    <div className={styles.vision}>
+                        <h2>Our Vision</h2>
+                    </div>
+                    <p className={styles.p1}>GrovNest aims to incentivize & tokenization platform for its users to
                          learn more about new digital capabilities and use them in their daily life.
                     </p>
                 </div>
-                <div className={styles.mission}>
-                    <h2>Our Mission</h2>
-                    <p>GrovNest's Mission is to become one-an-only paperless & e-platform with real values on each work/task 
+
+                <div>
+                    <div className={styles.vision}>
+                        <h2>Our Mission</h2>
+                    </div>
+                    <p className={styles.p1}>GrovNest's Mission is to become one-an-only paperless & e-platform with real values on each work/task 
                         within the network & provides counseling in timely manner to overcome challenge in every part of life 
                         such as education, health, wealth, love, relationship & inspiration.
                         We believe in this phrase - "Penny Penny makes Money"
